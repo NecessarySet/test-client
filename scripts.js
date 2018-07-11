ceeeecba09068ee06ed6180cfa005469ae59e6cd
NecessarySet <masry-family@outlook.com>
2018-07-11 11:13:47 -0300

    First add

---
+++ scripts.js
@@ -0,0 +1,49 @@
+const app = document.getElementById('root');
+
+const logo = document.createElement('img');
+logo.src = 'logo.png';
+
+const container = document.createElement('div');
+container.setAttribute('class', 'container');
+
+app.appendChild(logo);
+app.appendChild(container);
+
+var request = new XMLHttpRequest();
+request.open('GET', 'https://ghibliapi.herokuapp.com/films', true);
+request.onload = function () {
+
+  // Begin accessing JSON data here
+  var data = JSON.parse(this.response);
+  if (request.status >= 200 && request.status < 400) {
+    data.forEach(movie => {
+      const card = document.createElement('div');
+      card.setAttribute('class', 'card');
+     	card.setAttribute('id', movie.id);
+
+      const h1 = document.createElement('h1');
+      h1.textContent = movie.title;
+
+      const p = document.createElement('p');
+      movie.description = movie.description.substring(0, 300);
+      p.textContent = `${movie.description}...`;
+
+      const i = document.createElement('p');
+	i.textContent = `${movie.id}`;
+	
+
+      container.appendChild(card);
+      card.appendChild(h1);
+      card.appendChild(p);
+      card.appendChild(i);
+
+
+    });
+  } else {
+    const errorMessage = document.createElement('marquee');
+    errorMessage.textContent = `Gah, it's not working!`;
+    app.appendChild(errorMessage);
+  }
+}
+
+request.send();
